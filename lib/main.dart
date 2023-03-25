c9f206cf2e8d941e9101657615f297ddc23e28b2
praam <praa.m.2002@gmail.com>
2023-03-25 21:50:59 +0300

    ButtonNa

--- lib/main.dart
+++ lib/main.dart
@@ -71,7 +71,7 @@ class MyApp extends StatelessWidget {
               Locale('ar', ''), // Arabic
               Locale('he', ''), // Hebrew
             ],
-            locale: const Locale('he', ''));
+            locale: const Locale('en', ''));
       },
     );
   }
